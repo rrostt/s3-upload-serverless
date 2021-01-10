2f808cae2b9b242ae29e5a34931d8e2158df5c5d
Mattias Rost <rrostt@gmail.com>
2021-01-10 20:24:44 +0100

    Add plantId

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const s3 = new AWS.S3({
 
 const getPresignedUrl = (req, res) => {
   console.log(req.body)
+  const plantId = req.body.plantId
 
   let fileType = req.body.fileType;
   if (fileType != ".jpg" && fileType != ".png" && fileType != ".jpeg") {
@@ -33,12 +34,15 @@ const getPresignedUrl = (req, res) => {
 
   fileType = fileType.substring(1, fileType.length);
 
-  const fileName = uuid.v4();
+  const imageId = uuid.v4();
+  const fileName = plantId ? `plants/${plantId}/${imageId}` : imageId
+
+  console.log({ fileName })
   const s3Params = {
     Bucket: S3_BUCKET,
-    Key: fileName + "." + fileType,
+    Key: `${fileName}.${fileType}`,
     Expires: 60 * 60,
-    ContentType: "image/" + fileType,
+    ContentType: `image/${fileType}`,
     ACL: "public-read",
   };
 
@@ -59,47 +63,70 @@ const getPresignedUrl = (req, res) => {
 };
 
 const getLatestImage = (req, res) => {
-  const bucketParams = {
-    Bucket: S3_BUCKET,
-  };
-  s3.listObjects(bucketParams, (err, data) => {
-    const files = data.Contents.sort((a, b) => a.LastModified < b.LastModified ? -1 : a.LastModified > b.LastModified ? 1 : 0)
-    console.log(files[files.length - 1])
-    const latest = files[files.length - 1]
-    const url = `https://${S3_BUCKET}.s3.amazonaws.com/${latest.Key}`
-    if (req.query.redirect) {
-      res.redirect(url)
-    } else if (req.query.image) {
-      fetch(url)
-        .then(response => response.buffer())
-        .then(buffer => {
-          res.set({ 'Content-Type': 'image/png' }).send(buffer)
-        })
-    } else {
-      res.status(200).json({ url: url })
-    }
-  })
+  const { plantId } = req.query
+
+  listPlantImages({ from: '2020-01-01', to: new Date(), plantId })
+    .then(images => {
+      if (images.length === 0) {
+        res.status(200).json({})
+        return
+      }
+      const latest = images.sort((a, b) => a.LastModified < b.LastModified ? -1 : a.LastModified > b.LastModified ? 1 : 0)[images.length - 1]
+      const url = latest.url
+      if (req.query.redirect) {
+        res.redirect(url)
+      } else if (req.query.image) {
+        fetch(url)
+          .then(response => response.buffer())
+          .then(buffer => {
+            res.set({ 'Content-Type': 'image/png' }).send(buffer)
+          })
+      } else {
+        res.status(200).json({ url: url })
+      }
+    })
 }
 
+const listPlantImages = ({ from, to, plantId }) =>
+  new Promise((resolve, resject) => {
+    const bucketParams = {
+      Bucket: S3_BUCKET,
+    };
+    if (plantId) {
+      bucketParams.Prefix = `plants/${plantId}/`
+    }
+    let images = []
+    const getImages = () => {
+      s3.listObjectsV2(bucketParams, (err, data) => {
+        if (err) reject(err)
+
+        images.push(...data.Contents
+          .filter(({ LastModified: date }) => new Date(date) >= new Date(from) && new Date(date) <= new Date(to))
+          .filter(({ Key }) => plantId || !Key.startsWith('plants/'))
+          .filter(({ Key }) => Key.endsWith('.png'))
+          .sort((a, b) => a.LastModified < b.LastModified ? -1 : a.LastModified > b.LastModified ? 1 : 0)
+          .map(({ Key, LastModified }) => ({
+            time: LastModified,
+            url: `https://${S3_BUCKET}.s3.amazonaws.com/${Key}`
+          })))
+        if (data.NextContinuationToken) {
+          bucketParams.ContinuationToken = data.NextContinuationToken
+          getImages()
+        } else {
+          resolve(images)
+        }
+      })
+    }
+    getImages()
+  })
+
 const getImages = (req, res) => {
-  const { from, to } = req.query
+  const { from, to, plantId } = req.query
 
   console.log(from, to, new Date(from), new Date(to))
 
-  const bucketParams = {
-    Bucket: S3_BUCKET,
-  };
-  s3.listObjects(bucketParams, (err, data) => {
-    const images = data.Contents
-      .filter(({ LastModified: date }) => new Date(date) >= new Date(from) && new Date(date) <= new Date(to))
-      .filter(({ Key }) => Key.endsWith('.png'))
-      .sort((a, b) => a.LastModified < b.LastModified ? -1 : a.LastModified > b.LastModified ? 1 : 0)
-      .map(({ Key, LastModified }) => ({
-        time: LastModified,
-        url: `https://${S3_BUCKET}.s3.amazonaws.com/${Key}`
-      }))
-    res.json(images)
-  })
+  listPlantImages({ from, to, plantId })
+    .then(images => res.json(images))
 }
 
 app.post("/generatePresignedUrl", getPresignedUrl);
